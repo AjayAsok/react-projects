b5ff1a3855203846d36b555b0c6eaae4414a0d17
Ajay Asok <ajayasok007@gmail.com>
2021-02-26 11:34:10 +0530

    removed changes

--- React-TODO-App/src/styledComponents/Bootstrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Bootstrap(){
-    return(
-      <div>
-          <button className="btn btn-info">Click Me.</button>
-      </div>  
-    );
-}
--- React-TODO-App/src/styledComponents/Content.js
+++
@@ -1,4 +0,0 @@
-import React,{useState} from 'react'
-export default function Content(props){
-    return <h1 className={props.Color}>The Count is {props.Counter}</h1>;
-}
